7312770198be23d37bfda99b7e07bb2b0d8f484d
bujin21 <cvj0810@naver.com>
2025-11-15 18:45:39 +0900

    AWS Lambda + API Gateway 서버리스 아키텍처 완료
    - Lambda 함수 7개 생성 (인증 2개, 레시피 CRUD 5개)
    - Lambda Layer로 npm 패키지 관리
    - DynamoDB 단일 테이블 설계 적용
    - API Gateway REST API 생성 및 배포
    - CORS 설정 완료
    - 프론트엔드 API 엔드포인트 연결 (API Gateway URL)
    - API 서비스 파일 구조화 (api/index.js, auth.js, recipes.js)

---
+++ frontend/src/api/recipes.js
@@ -0,0 +1,26 @@
+import api from './index';
+
+export const getRecipes = async () => {
+  const response = await api.get('/recipes');
+  return response.data;
+};
+
+export const getRecipe = async (id) => {
+  const response = await api.get(`/recipes/${id}`);
+  return response.data;
+};
+
+export const createRecipe = async (recipeData) => {
+  const response = await api.post('/recipes', recipeData);
+  return response.data;
+};
+
+export const updateRecipe = async (id, recipeData) => {
+  const response = await api.put(`/recipes/${id}`, recipeData);
+  return response.data;
+};
+
+export const deleteRecipe = async (id) => {
+  const response = await api.delete(`/recipes/${id}`);
+  return response.data;
+};
